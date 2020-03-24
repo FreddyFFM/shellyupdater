374a63f409cd8b3f910fe0a0c7ebd1f850a45111
mfraedrich <michael.fraedrich@gls-ecl.com>
2020-03-24 17:48:05 +0100

    Adding Dashboard

--- shellyupdater/updates/urls.py
+++ shellyupdater/updates/urls.py
@@ -1,13 +1,16 @@
-from .views import ShowShelliesView, OpenhabThingsView, ShellyDetailView
+from .views import ShowShelliesView, OpenhabThingsView, ShellyDetailView, HomeView
 from django.urls import path, re_path
 
 urlpatterns = [
-    path('', ShowShelliesView.as_view(), name="home"),
-    re_path('^(?P<refresh>[Y]+)/$', ShowShelliesView.as_view(), name="home"),
+    path('', HomeView.as_view(), name="home"),
+
+    path('shellies', ShowShelliesView.as_view(), name="shellies"),
+    re_path('^shellies/(?P<refresh>[Y]+)/$', ShowShelliesView.as_view(), name="shellies"),
+
+    re_path('^shellies/details/(?P<shelly_id>[\w\-]+)/$', ShellyDetailView.as_view(), name="shellies/details"),
+    re_path('^shellies/details/(?P<shelly_id>[\w\-]+)/(?P<refresh>[Y]+)/$', ShellyDetailView.as_view(), name="shellies/details"),
 
     path('things', OpenhabThingsView.as_view(), name="things"),
     re_path('^things/(?P<refresh>[Y]+)/$', OpenhabThingsView.as_view(), name="things"),
 
-    re_path('^details/(?P<shelly_id>[\w\-]+)/$', ShellyDetailView.as_view(), name="details"),
-    re_path('^details/(?P<shelly_id>[\w\-]+)/(?P<refresh>[Y]+)/$', ShellyDetailView.as_view(), name="details"),
 ]
--- shellyupdater/updates/views.py
+++ shellyupdater/updates/views.py
@@ -11,6 +11,33 @@ from datetime import datetime
 from shellyupdater.mqtt import get_mqttclient
 from .shelly_http_handler import get_shelly_info
 from django.http import HttpResponse
+from django.db.models import Avg, Max, Min, Count
+
+
+class HomeView(TemplateView):
+
+    template_name = 'home.html'
+
+    def get(self, request, refresh=None, *args, **kwargs):
+        context = {}
+
+        shelly_info = Shellies.objects.all().aggregate(Count('shelly_id'),
+                                                            Max('shelly2infos__shelly_battery_percent'),
+                                                            Min('shelly2infos__shelly_battery_percent'),
+                                                            Avg('shelly2infos__shelly_battery_percent'),
+                                                         )
+        context["shelly_info"] = shelly_info
+
+        shelly_oldest = Shellies.objects.earliest('last_change_ts')
+        context["shelly_oldest"] = shelly_oldest
+
+        shellies_wo_things = Shellies.objects.filter(shelly2thing__thing_uid__isnull=True)
+        things_wo_shelly = OpenHabThings.objects.filter(shelly_id__isnull=True)
+        context["shellies_wo_things"] = shellies_wo_things
+        context["things_wo_shelly"] = things_wo_shelly
+
+
+        return self.render_to_response(context)
 
 
 class ShowShelliesView(TemplateView):
