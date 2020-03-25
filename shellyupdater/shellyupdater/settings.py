e82ce688957a0ceeb798b848f478ed66c5545941
mfraedrich <michael.fraedrich@gls-ecl.com>
2020-03-25 12:08:48 +0100

    Fix in DB DIR Settings

--- shellyupdater/shellyupdater/settings.py
+++ shellyupdater/shellyupdater/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'shellyupdater.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(env.str('DATABASE_DIR', BASE_DIR), 'db.sqlite3'),
     }
 }
 
